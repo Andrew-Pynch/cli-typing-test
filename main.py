745bcd3ead6ac2192d506a0196d98ace41f44a75
Andrew Pynch <andrewpynchbusiness@gmail.com>
2021-02-12 17:02:35 -0800

    finish

--- main.py
+++ main.py
@@ -2,20 +2,31 @@ import os
 import sys
 import time
 import random
+from util.util import get_remaining_time
 from word.wordmanager import WordManager
-from util.timer import Timer
+from util import *
 
 
 def main():
     manager = WordManager()
     clear = lambda: os.system("clear")  # on Linux System
-    # timer = Timer(60)
-    # timer.countdown()
-    while True:
+
+    print("To being, press space")
+    start = manager.get_custom_input()
+    start_time = time.time()
+    remaining_time = get_remaining_time(start_time)
+    while remaining_time <= 60:
+        clear()
+        print(f"{remaining_time} seconds\n")
         manager.print_current_words()
         user_word = manager.get_custom_input()
         manager.manage_word_input(user_word)
         clear()
+    handle_post_run(manager)
+
+
+def handle_post_run(manager):
+    m
 
 
 if __name__ == "__main__":
---
+++ util/util.py
@@ -0,0 +1,7 @@
+import sys
+import time
+import random
+
+
+def get_remaining_time(start_time):
+    return round((time.time() - start_time), 0)
